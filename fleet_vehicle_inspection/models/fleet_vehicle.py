23d30167909b48d3a473c62dc1842432eabd400c
Marcel Savegnago <marcel.savegnago@gmail.com>
2021-02-28 15:24:05 -0300

    [MIG] fleet_vehicle_inspection: Migration to 13.0
    [UPD] README.rst
    
    [UPD] Update fleet_vehicle_inspection.pot

--- fleet_vehicle_inspection/models/fleet_vehicle.py
+++ fleet_vehicle_inspection/models/fleet_vehicle.py
@@ -6,36 +6,37 @@ from odoo import api, fields, models
 
 class FleetVehicle(models.Model):
 
-    _inherit = 'fleet.vehicle'
+    _inherit = "fleet.vehicle"
 
     inspection_ids = fields.One2many(
-        'fleet.vehicle.inspection',
-        'vehicle_id',
-        'Inspection Logs'
+        "fleet.vehicle.inspection", "vehicle_id", "Inspection Logs"
     )
 
     inspection_count = fields.Integer(
-        compute="_compute_inspection_count",
-        string='# Inspection Count')
+        compute="_compute_inspection_count", string="# Inspection Count"
+    )
 
-    @api.depends('inspection_ids')
+    @api.depends("inspection_ids")
     def _compute_inspection_count(self):
         for rec in self:
-            rec.inspection_count = len(
-                rec.inspection_ids)
+            rec.inspection_count = len(rec.inspection_ids)
 
-    @api.multi
     def action_view_inspection(self):
         action = self.env.ref(
-            "fleet_vehicle_inspection.fleet_vehicle_inspection_act_window").read()[0]
+            "fleet_vehicle_inspection.fleet_vehicle_inspection_act_window"
+        ).read()[0]
         if self.inspection_count > 1:
             action["domain"] = [("id", "in", self.inspection_ids.ids)]
         else:
-            action["views"] = [(
-                self.env.ref(
-                    "fleet_vehicle_inspection.fleet_vehicle_inspection_form_view").id,
-                "form"
-            )]
-            action["res_id"] = \
+            action["views"] = [
+                (
+                    self.env.ref(
+                        "fleet_vehicle_inspection.fleet_vehicle_inspection_form_view"
+                    ).id,
+                    "form",
+                )
+            ]
+            action["res_id"] = (
                 self.inspection_ids and self.inspection_ids.ids[0] or False
+            )
         return action
