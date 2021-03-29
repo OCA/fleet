5e42a87f3aa149d59d1c90f0eca9c1303ac5f8e8
Marcel Savegnago <marcel.savegnago@gmail.com>
2021-03-29 01:01:25 -0300

    [ADD] fleet_vehicle_inspection_item_product

---
+++ fleet_vehicle_inspection_item_product/__manifest__.py
@@ -0,0 +1,25 @@
+# Copyright 2021 - TODAY, Escodoo
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Fleet Vehicle Inspection Item Product',
+    'summary': """
+        This module is a bridge between Inspection Item with Product""",
+    'version': '12.0.1.0.0',
+    'category': 'Human Resources/Fleet',
+    'license': 'AGPL-3',
+    'author': 'Escodoo,Odoo Community Association (OCA)',
+    'maintainers': ['marcelsavegnago'],
+    'images': ['static/description/banner.png'],
+    'website': 'https://github.com/OCA/fleet',
+    'depends': [
+        'fleet_product',
+        'fleet_vehicle_inspection',
+    ],
+    'data': [
+        'views/fleet_vehicle_inspection_item.xml',
+        'views/product_product.xml',
+    ],
+    'demo': [
+    ],
+}
---
+++ fleet_vehicle_inspection_item_product/models/__init__.py
@@ -0,0 +1,2 @@
+from . import product_product
+from . import fleet_vehicle_inspection_item
