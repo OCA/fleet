153f498f5d6161e8cd31a64e06078c2012f641ca
Marcel Savegnago <marcel.savegnago@gmail.com>
2021-12-19 17:27:00 -0300

    [IMP] fleet_rest_api: fleet_vehicle_info add fields

--- fleet_rest_api/pydantic_models/fleet_vehicle_info.py
+++ fleet_rest_api/pydantic_models/fleet_vehicle_info.py
@@ -1,7 +1,7 @@
 # Copyright 2021 - TODAY, Marcel Savegnago <marcel.savegnago@escodoo.com.br>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from datetime import datetime
+from datetime import date, datetime
 
 import pydantic
 
@@ -13,10 +13,50 @@ from .fleet_vehicle_state_info import FleetVehicleStateInfo
 class FleetVehicleInfo(models.BaseModel):
     id: int
     name: str
+    description: str
+    active: bool
+    company_id: int
+    currency_id: int
+    license_plate: str
+    vin_sn: str
+    driver_id: int
+    future_driver_id: int
+    model_id: int
+    manager_id: int
+    brand_id: int
+    # log_drivers
+    # log_services
+    # log_contracts
+    contract_count: int
+    service_count: int
+    odometer_count: int
+    history_count: int
+    next_assignation_date: date
+    acquisition_date: date
+    first_contract_date: date
+    color: str
+    state: FleetVehicleStateInfo = pydantic.Field(None, alias="state_id")
+    location: str
+    seats: int
+    model_year: str
+    # tag_ids
     odometer: float
     odometer_unit: str
+    transmission: str
+    fuel_type: str
+    horsepower: int
+    horsepower_tax: float
+    power: int
+    co2: float
+    contract_renewal_due_soon: bool
+    contract_renewal_overdue: bool
+    contract_renewal_name: str
+    contract_renewal_total: str
+    car_value: float
+    net_car_value: float
+    residual_value: float
+    plan_to_change_car: bool
     vehicle_type: str
-    state: FleetVehicleStateInfo = pydantic.Field(None, alias="state_id")
     write_date: datetime
 
     class Config:
