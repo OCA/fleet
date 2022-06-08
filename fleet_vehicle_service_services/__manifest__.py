d70d18fa34df595070e3bcd090953ef6540138e4
Miquel Raïch <miquel.raich@forgeflow.com>
2022-06-08 17:44:43 +0200

    [ADD] fleet_vehicle_service_services

---
+++ fleet_vehicle_service_services/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2022 ForgeFlow S.L.  <https://www.forgeflow.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Fleet Vehicle Service Services",
+    "summary": "Add subservices in Services.",
+    "version": "14.0.1.0.0",
+    "category": "Human Resources/Fleet",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/fleet",
+    "license": "AGPL-3",
+    "depends": ["fleet"],
+    "data": [
+        "views/fleet_vehicle_cost_views.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
