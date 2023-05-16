3edc958fae0e5ca9b587494ad0ee2059936734b3
Zina Rasoamanana <zina.rasoamanana@acsone.eu>
2023-05-16 09:53:08 +0200

    [MIG] fleet_vehicle_log_fuel: Migration to 16.0

--- fleet_vehicle_log_fuel/report/fleet_report.py
+++ fleet_vehicle_log_fuel/report/fleet_report.py
@@ -2,11 +2,15 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from psycopg2 import sql
 
-from odoo import models, tools
+from odoo import fields, models, tools
 
 
 class FleetReport(models.Model):
     _inherit = "fleet.vehicle.cost.report"
+
+    cost_type = fields.Selection(
+        selection_add=[("fuel", "Fuel Log")], ondelete={"fuel": "cascade"}
+    )
 
     def init(self):
         query = """
@@ -18,23 +22,27 @@ WITH service_costs AS (
         ve.driver_id AS driver_id,
         ve.fuel_type AS fuel_type,
         date(date_trunc('month', d)) AS date_start,
+        vem.vehicle_type as vehicle_type,
         COALESCE(sum(se.amount), 0) AS
         COST,
         'service' AS cost_type
     FROM
         fleet_vehicle ve
-    CROSS JOIN generate_series(
-        (
-            SELECT min(date)
-            FROM fleet_vehicle_log_services),
-        CURRENT_DATE + '1 month'::interval, '1 month') d
-    LEFT JOIN fleet_vehicle_log_services se ON se.vehicle_id = ve.id
-        AND date_trunc('month', se.date) = date_trunc('month', d)
+    JOIN
+        fleet_vehicle_model vem ON vem.id = ve.model_id
+    CROSS JOIN generate_series((
+            SELECT
+                min(date)
+                FROM fleet_vehicle_log_services),
+                CURRENT_DATE + '1 month'::interval, '1 month') d
+        LEFT JOIN fleet_vehicle_log_services se ON se.vehicle_id = ve.id
+            AND date_trunc('month', se.date) = date_trunc('month', d)
     WHERE
         ve.active AND se.active AND se.state != 'cancelled'
     GROUP BY
         ve.id,
         ve.company_id,
+        vem.vehicle_type,
         ve.name,
         date_start,
         d
@@ -42,7 +50,7 @@ WITH service_costs AS (
         ve.id,
         date_start
 ),
-fuel_costs AS (
+fuel_costs AS(
     SELECT
         ve.id AS vehicle_id,
         ve.company_id AS company_id,
@@ -50,23 +58,26 @@ fuel_costs AS (
         ve.driver_id AS driver_id,
         ve.fuel_type AS fuel_type,
         date(date_trunc('month', d)) AS date_start,
+        vem.vehicle_type as vehicle_type,
         COALESCE(sum(se.amount), 0) AS
         COST,
         'fuel' AS cost_type
     FROM
         fleet_vehicle ve
-    CROSS JOIN generate_series(
-        (
-            SELECT min(date)
-            FROM fleet_vehicle_log_fuel),
-        CURRENT_DATE + '1 month'::interval, '1 month') d
-    LEFT JOIN fleet_vehicle_log_fuel se ON se.vehicle_id = ve.id
-        AND date_trunc('month', se.date) = date_trunc('month', d)
+    JOIN
+        fleet_vehicle_model vem ON vem.id = ve.model_id
+    CROSS JOIN generate_series((
+            SELECT
+                min(date)
+                FROM fleet_vehicle_log_fuel), CURRENT_DATE + '1 month'::interval, '1 month') d
+        LEFT JOIN fleet_vehicle_log_fuel se ON se.vehicle_id = ve.id
+            AND date_trunc('month', se.date) = date_trunc('month', d)
     WHERE
         ve.active AND se.active AND se.state != 'cancelled'
     GROUP BY
         ve.id,
         ve.company_id,
+        vem.vehicle_type,
         ve.name,
         date_start,
         d
@@ -82,23 +93,20 @@ contract_costs AS (
         ve.driver_id AS driver_id,
         ve.fuel_type AS fuel_type,
         date(date_trunc('month', d)) AS date_start,
-        (
-            COALESCE(
-                sum(co.amount), 0
-            ) + COALESCE(
-                sum(cod.cost_generated * extract(day FROM
-                    least (date_trunc('month', d) + interval '1 month', cod.expiration_date
-                        ) - greatest (date_trunc('month', d), cod.start_date)
-                )), 0
-            ) + COALESCE(
-                sum(com.cost_generated), 0
-            ) + COALESCE(
-                sum(coy.cost_generated), 0
-            )
-        ) AS COST,
+        vem.vehicle_type as vehicle_type,
+        (COALESCE(sum(co.amount), 0)
+          + COALESCE(sum(cod.cost_generated
+            * extract(day
+              FROM least (date_trunc('month', d) + interval '1 month', cod.expiration_date)
+              - greatest (date_trunc('month', d), cod.start_date))), 0)
+              + COALESCE(sum(com.cost_generated), 0)
+              + COALESCE(sum(coy.cost_generated), 0)) AS
+        COST,
         'contract' AS cost_type
     FROM
         fleet_vehicle ve
+    JOIN
+        fleet_vehicle_model vem ON vem.id = ve.model_id
     CROSS JOIN generate_series((
             SELECT
                 min(acquisition_date)
@@ -118,56 +126,69 @@ contract_costs AS (
         AND date_trunc('month', coy.start_date) <= date_trunc('month', d)
         AND date_trunc('month', coy.expiration_date) >= date_trunc('month', d)
         AND coy.cost_frequency = 'yearly'
-WHERE
-    ve.active
-GROUP BY
-    ve.id,
-    ve.company_id,
-    ve.name,
-    date_start,
-    d
-ORDER BY
-    ve.id,
-    date_start
+    WHERE
+        ve.active
+    GROUP BY
+        ve.id,
+        ve.company_id,
+        vem.vehicle_type,
+        ve.name,
+        date_start,
+        d
+    ORDER BY
+        ve.id,
+        date_start
 )
-SELECT
-    vehicle_id AS id,
+SELECT row_number() OVER (ORDER BY vehicle_id ASC) as id,
     company_id,
     vehicle_id,
     name,
     driver_id,
     fuel_type,
     date_start,
+    vehicle_type,
     COST,
-    'service' as cost_type
-FROM
-    service_costs sc
-UNION ALL (
+    cost_type
+FROM (
     SELECT
-        vehicle_id AS id,
         company_id,
         vehicle_id,
         name,
         driver_id,
         fuel_type,
         date_start,
+        vehicle_type,
         COST,
-        'fuel' as cost_type
+        'service' as cost_type
     FROM
-        fuel_costs cc)
-UNION ALL (
-    SELECT
-        vehicle_id AS id,
-        company_id,
-        vehicle_id,
-        name,
-        driver_id,
-        fuel_type,
-        date_start,
-        COST,
-        'contract' as cost_type
-    FROM
-        contract_costs cc)
+        service_costs sc
+    UNION ALL (
+        SELECT
+            company_id,
+            vehicle_id,
+            name,
+            driver_id,
+            fuel_type,
+            date_start,
+            vehicle_type,
+            COST,
+            'fuel' as cost_type
+        FROM
+            fuel_costs fc)
+    UNION ALL (
+        SELECT
+            company_id,
+            vehicle_id,
+            name,
+            driver_id,
+            fuel_type,
+            date_start,
+            vehicle_type,
+            COST,
+            'contract' as cost_type
+        FROM
+            contract_costs cc)
+) c
 """
         tools.drop_view_if_exists(self.env.cr, self._table)
         self.env.cr.execute(
