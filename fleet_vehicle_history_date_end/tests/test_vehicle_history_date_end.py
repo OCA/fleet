560927810cbe00bb3bd11816f76c492d1678144b
kaynnan <kaynnan.lemes@escodoo.com.br>
2023-07-11 05:53:30 -0300

    [MIG] fleet_vehicle_history_date_end: Migration to 15.0

--- fleet_vehicle_history_date_end/tests/test_vehicle_history_date_end.py
+++ fleet_vehicle_history_date_end/tests/test_vehicle_history_date_end.py
@@ -1,27 +1,14 @@
 # Copyright 2020-Present Druidoo - Manuel Marquez <manuel.marquez@druidoo.io>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo.tests import tagged
 from odoo.tests.common import TransactionCase
 
 
-@tagged("-standard")
 class TestFleetVehicleDateEnd(TransactionCase):
-    def setUp(self):
-        super().setUp()
-        self.vehicle = self.env["fleet.vehicle"].create(
-            {
-                "license_plate": "1-ACK-555",
-                "vin_sn": "883333",
-                "color": "Black",
-                "location": "Grand-Rosiere",
-                "doors": 5,
-                "driver_id": self.env.ref("base.user_demo_res_partner").id,
-                "odometer_unit": "kilometers",
-                "car_value": 20000,
-                "model_id": self.env.ref("fleet.model_astra").id,
-            }
-        )
+    @classmethod
+    def setUpClass(cls):
+        super(TestFleetVehicleDateEnd, cls).setUpClass()
+        cls.vehicle = cls.env.ref("fleet.vehicle_1")
 
     def test_change_driver_history_date_end(self):
         """Check correct assignation of date_end in history of previous driver."""
