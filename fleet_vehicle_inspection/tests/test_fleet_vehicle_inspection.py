002b61219819f484ccb894ce5de5db1bb26945d6
Carolina Fernandez <carolina.fernandez@tecnativa.com>
2023-12-14 18:10:43 -0300

    [15.0][IMP] fleet_vehicle_inspection: Add cost vehicle inspection and create vehicle service
    [UPD] Update fleet_vehicle_inspection.pot
    
    [BOT] post-merge updates
    
    Update translation files
    
    Updated by "Update PO files to match POT (msgmerge)" hook in Weblate.
    
    Translation: fleet-15.0/fleet-15.0-fleet_vehicle_inspection
    Translate-URL: https://translation.odoo-community.org/projects/fleet-15-0/fleet-15-0-fleet_vehicle_inspection/

--- fleet_vehicle_inspection/tests/test_fleet_vehicle_inspection.py
+++ fleet_vehicle_inspection/tests/test_fleet_vehicle_inspection.py
@@ -1,11 +1,12 @@
 # Copyright 2020 - TODAY, Marcel Savegnago - Escodoo
+# Copyright 2023 Tecnativa - Carolina Fernandez
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl
 
-from odoo.exceptions import UserError
-from odoo.tests import SavepointCase
+from odoo.exceptions import UserError, ValidationError
+from odoo.tests import TransactionCase
 
 
-class TestFleetVehicleInspection(SavepointCase):
+class TestFleetVehicleInspection(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super(TestFleetVehicleInspection, cls).setUpClass()
@@ -79,3 +80,30 @@ class TestFleetVehicleInspection(SavepointCase):
         # inspection2 confirm without items
         with self.assertRaises(UserError):
             self.inspection2.button_confirm()
+
+    def test_fleet_vehicle_inspection_with_amount(self):
+        test_vendor = self.env["res.partner"].create({"name": "test vendor"})
+        self.inspection.inspection_line_ids[0].action_item_success()
+        self.inspection.inspection_line_ids[1].action_item_success()
+        self.inspection._compute_inspection_result()
+        self.inspection.amount = 100
+        self.inspection.vendor_id = test_vendor.id
+        with self.assertRaisesRegex(ValidationError, "Must select service type"):
+            self.inspection.button_confirm()
+        self.inspection.service_type_id = self.env.ref("fleet.type_service_service_8")
+        self.inspection.button_confirm()
+        self.assertEqual(self.inspection.service_id.description, self.inspection.name)
+        self.assertEqual(
+            self.inspection.service_id.vendor_id, self.inspection.vendor_id
+        )
+        self.assertEqual(
+            self.inspection.service_id.vehicle_id, self.inspection.vehicle_id
+        )
+        self.assertEqual(self.inspection.service_id.odometer, self.inspection.odometer)
+        self.assertEqual(
+            self.inspection.service_id.service_type_id, self.inspection.service_type_id
+        )
+        self.assertEqual(self.inspection.service_id.amount, self.inspection.amount)
+        self.assertEqual(self.inspection.service_id.state, "done")
+        self.inspection.button_cancel()
+        self.assertFalse(self.inspection.service_id)
