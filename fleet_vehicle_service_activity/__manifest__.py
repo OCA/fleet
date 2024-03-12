baabe526bcf5076ea757f39b770dd22d8af83f23
Carolina Fernandez <carolina.fernandez@tecnativa.com>
2024-03-12 16:15:49 +0100

    [MIG] fleet_vehicle_service_activity: Migration to 16.0

--- fleet_vehicle_service_activity/__manifest__.py
+++ fleet_vehicle_service_activity/__manifest__.py
@@ -9,7 +9,7 @@
     "development_status": "Production/Stable",
     "website": "https://github.com/OCA/fleet",
     "license": "AGPL-3",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "depends": ["fleet"],
     "data": [
         "data/fleet_data.xml",
