30f5e8361695290d7f244a51add71de23a21f746
cubells <vicent@vcubells.net>
2025-06-21 10:03:59 +0200

    [MIG] fleet_vehicle_ownership: Migration to 16.0

--- fleet_vehicle_ownership/__manifest__.py
+++ fleet_vehicle_ownership/__manifest__.py
@@ -1,17 +1,17 @@
 # Copyright 2023 RPSJR
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-
 {
     "name": "Fleet Vehicle Ownership",
     "summary": "Add vehicle ownership, linking partners to vehicles",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "RPSJR,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/fleet",
+    "maintainers": ["cubells"],
     "depends": ["fleet"],
     "data": [
-        "views/res_partner.xml",
-        "views/fleet_vehicle.xml",
+        "views/res_partner_views.xml",
+        "views/fleet_vehicle_views.xml",
     ],
     "demo": [
         "demo/fleet_vehicle.xml",
